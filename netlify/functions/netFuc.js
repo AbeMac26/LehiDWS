33e7d425de03503e62c21eecdc53c63ce8bf0390
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-07 12:53:42 -0600

    Update netFuc.js

--- netlify/functions/netFuc.js
+++ netlify/functions/netFuc.js
@@ -2,12 +2,12 @@
 //require('dotenv').config();
 //const{
 const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
-const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY';
+const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
 //} = process.env;
 
 
 //const {createClient} = require('@supabase/supabase-js');
-const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
 
 async function loadData() {
 	const { data, error } = await _supabase
@@ -16,7 +16,7 @@ async function loadData() {
 			{ item1: '100'},
 		]);
 
-console.log(data, error);
+console.log(data, error)
 
 }	
 loadData()
