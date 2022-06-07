737655d00606487d7373d0e49cf90a0aac99cf6b
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-07 12:52:44 -0600

    Update netFuc.js

--- netlify/functions/netFuc.js
+++ netlify/functions/netFuc.js
@@ -1,9 +1,9 @@
 
-require('dotenv').config();
-const{
-	DATABASE_URL,
-	SUPABASE_SERVICE_API_KEY
-} = process.env;
+//require('dotenv').config();
+//const{
+const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
+const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY';
+//} = process.env;
 
 
 //const {createClient} = require('@supabase/supabase-js');
