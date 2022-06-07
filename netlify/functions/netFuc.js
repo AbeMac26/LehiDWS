e9a62271a3e2469d13cbfb909ee8019a361308db
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-07 12:11:20 -0600

    Update netFuc.js

--- netlify/functions/netFuc.js
+++ netlify/functions/netFuc.js
@@ -1,14 +1,9 @@
 
-
-const DATABASE_URL = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1MzU2OTMsImV4cCI6MTk2OTExMTY5M30.-NiGB22jxJEMZUVFZ6MwjG7Q5a8otKZuVpFC7hH4X_s;
-
-const SUPABASE_SERVICE_API_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MzUzNTY5MywiZXhwIjoxOTY5MTExNjkzfQ.C1d-ROdD7O-RiWZ-rVzcsta8gw3Hz06gjqktwtrVJMA;
-
-//require('dotenv').config();
-//const{
-//	DATABASE_URL,
-//	SUPABASE_SERVICE_API_KEY
-//} = process.env;
+require('dotenv').config();
+const{
+	DATABASE_URL,
+	SUPABASE_SERVICE_API_KEY
+} = process.env;
 
 
 const {createClient} = require('@supabase/supabase-js');
