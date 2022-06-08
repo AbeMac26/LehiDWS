e1ff04299c254b7260e741b40354abd6c5b8ccc3
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-08 16:09:32 -0600

    Update loadData.js

--- loadData.js
+++ loadData.js
@@ -1,9 +1,9 @@
-//require('dotenv').config();
-//const{
 const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
 const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
-//} = process.env;
- var inName = document.getElementById("uname").value;
+const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
+
+async function loadData() {
+var inName = document.getElementById("uname").value;
  var inMonth = document.getElementById("month").value;
  var inDate = document.getElementById("date").value;
  var inItem1 = document.getElementById("item1").value; 
@@ -21,11 +21,7 @@ const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiO
  var inItem13 = document.getElementById("item13").value;
  var inItem14 = document.getElementById("item14").value;
 console.log(inName)
-//const {createClient} = require('@supabase/supabase-js');
-const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
-
-async function loadData() {
-
+	
 	const { data, error } = await _supabase
   		.from('CnE')
   		.update([{ Month: inMonth, Date: inDate , item1:'inItem1',item2:'inItem2',item3:'inItem3', item4:'inItem4'}])
