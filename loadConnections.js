6b057ee26e1bd466e65fd193cb8b0b83c388f063
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-09 09:50:44 -0600

    Update loadConnections.js

--- loadConnections.js
+++ loadConnections.js
@@ -1,5 +1,5 @@
 const worker1 = 'Aaron Aders'
-const worker2 = 'Carrie Burnham'
+const worker2 = 'Carie Burnham'
 const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
 const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
 const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
