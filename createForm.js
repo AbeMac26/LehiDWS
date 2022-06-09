4ba755388d9ca91b6766623b9e2f53e43f80c3ed
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-09 14:36:53 -0600

    Update createForm.js

--- createForm.js
+++ createForm.js
@@ -2,6 +2,19 @@ const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
 const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
 const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
 
+function optionList() {
+  var outName = JSON.parse(localStorage.getItem("counselorNames"))
+  var subjectSel = document.getElementById("counselor")
+  var z = outName
+  console.log(z)
+  for (var i = 0; i < z.length; i++){
+    subjectSel.options[subjectSel.options.length] = new Option(z[i], z[i])
+  }
+  subjectSel.onchange = function() {
+    //display correct values
+  
+  }
+}
 
 async function createList() {
     var counselorNames = [] 
@@ -25,19 +38,7 @@ async function createList() {
 }
    
   
-function optionList() {
-  var outName = JSON.parse(localStorage.getItem("counselorNames"))
-  var subjectSel = document.getElementById("counselor")
-  var z = outName
-  console.log(z)
-  for (var i = 0; i < z.length; i++){
-    subjectSel.options[subjectSel.options.length] = new Option(z[i], z[i])
-  }
-  subjectSel.onchange = function() {
-    //display correct values
-  
-  }
-}
+
 createList()
 optionList()
 
