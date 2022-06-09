d126b97f9aa2ddea1ec40d35f53285995b685a3f
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-09 12:16:07 -0600

    Update createForm.js

--- createForm.js
+++ createForm.js
@@ -2,7 +2,7 @@ var subjectObject = ["Links", "Images", "Tables", "Lists"]
   
 window.onload = function() {
   var subjectSel = document.getElementById("counselor")
-  var z = subjectObject[subjectSel.value]
+  var z = subjectObject[this.value]
   for (var i = 0; i < z.length; i++){
     subjectSel.options[subjectSel.options.length] = new Option(z[i], z[i])
   }
