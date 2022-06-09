5ad2702d74e653dd0f7ae41bd8b3601711dc28fe
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-09 12:12:50 -0600

    Update createForm.js

--- createForm.js
+++ createForm.js
@@ -1,10 +1,10 @@
 var subjectObject = ["Links", "Images", "Tables", "Lists"]  
   
 window.onload = function() {
-  var subjectSel = document.getElementById("counselor");
-  var z = subjectObject[subjectSel.value][this.value];
-  for (var i = 0; i < z.length; i++)
-    subjectSel.options[subjectSel.options.length] = new Option(z[i], z[i]);
+  var subjectSel = document.getElementById("counselor")
+  var z = subjectObject[subjectSel.value][this.value]
+  for (var i = 0; i < z.length; i++){
+    subjectSel.options[subjectSel.options.length] = new Option(z[i], z[i])
   }
   subjectSel.onchange = function() {
     //display correct values
