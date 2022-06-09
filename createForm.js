3e8007dcef5b8209f68f51f327ecb40808a14424
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-09 14:29:31 -0600

    Update createForm.js

--- createForm.js
+++ createForm.js
@@ -1,11 +1,13 @@
 const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
 const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
 const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
-var counselorNames = [] 
-localStorage.setItem('counselorNames',JSON.stringify(counselorNames))
-var outName = JSON.parse(localStorage.getItem("counselorNames"))
+
 
 async function createList() {
+    var counselorNames = [] 
+    localStorage.setItem('counselorNames',JSON.stringify(counselorNames))
+    var outName = JSON.parse(localStorage.getItem("counselorNames"))
+    
     const { data, error } = await _supabase
             .from('CnE')
             .select('Name')
@@ -24,7 +26,7 @@ async function createList() {
    createList()
   
 function optionList() {
-  
+  var outName = JSON.parse(localStorage.getItem("counselorNames"))
   var subjectSel = document.getElementById("counselor")
   var z = outName
   console.log(z)
