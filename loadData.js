af29c835725ab57dd535be9ae8ec26947d7c2a2d
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-09 07:38:06 -0600

    Update loadData.js

--- loadData.js
+++ loadData.js
@@ -1,6 +1,6 @@
-//const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
-//const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
-const _supabase = supabase.createClient(env.DATABASE_URL, env.SUPABASE_SERVICE_API_KEY)
+Private	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
+Private	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
+const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
 
 async function loadData() {
 var inName = document.getElementById("uname").value;
