61313c6b8abd2169e46f984837adbe50ead868c3
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-09 14:20:50 -0600

    Update createForm.js

--- createForm.js
+++ createForm.js
@@ -2,19 +2,19 @@ const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
 const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
 const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
 var counselorNames = [] 
-localStorage.setItem('worker1',JSON.stringify(counselorNames))
+localStorage.setItem('counselorNames',JSON.stringify(counselorNames))
+var outName = JSON.parse(localStorage.getItem("counselorNames"))
 
 async function createList() {
     const { data, error } = await _supabase
             .from('CnE')
             .select('Name')
             .eq('completed', true)
-    
-     var outName = localStorage.getItem("counselorNames");
+   
     if(!error) {
         //loop display data here
         data.forEach(function(item){
-            subjectObject.push(item.Name)  
+            outName.push(item.Name)  
         })
     }
     console.log(data)
@@ -25,7 +25,7 @@ async function createList() {
 window.onload = function() {
   
   var subjectSel = document.getElementById("counselor")
-  var z = subjectObject
+  var z = outName
   console.log(z)
   for (var i = 0; i < z.length; i++){
     subjectSel.options[subjectSel.options.length] = new Option(z[i], z[i])
