68bc25fcb3291b27715318a731892643bf5172e4
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-09 12:34:20 -0600

    Update createForm.js

--- createForm.js
+++ createForm.js
@@ -1,6 +1,26 @@
-var subjectObject = ["Links", "Images", "Tables", "Lists"]  
+const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
+const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
+const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
+var subjectObject = [] 
+
+async function createList() {
+    const { data, error } = await _supabase
+            .from('CnE')
+            .select('Name')
+            .eq('completed', true)
+    
+    if(!error) {
+        //loop display data here
+        data.forEach(function(item){
+            subjectObject.push(item.Name)  
+        })
+      
+    console.log(data)
+    console.log(error)
+}
   
 window.onload = function() {
+  createList()  
   var subjectSel = document.getElementById("counselor")
   var z = subjectObject
   console.log(z)
