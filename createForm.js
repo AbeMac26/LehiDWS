0c7e53e7a1bbb1b8fbe974962c2de66bffc65976
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-10 07:32:10 -0600

    Update createForm.js

--- createForm.js
+++ createForm.js
@@ -2,7 +2,7 @@ const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
 const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
 const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
 
-async function optionList() {
+function optionList() {
   var out = JSON.parse(sessionStorage.getItem("counselorNames"))
   var subjectSel = document.getElementById("counselor")
   var z = out
