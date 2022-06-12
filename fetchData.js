5d580fa886eeea928b204f413b0bcf46ee1519d0
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-12 10:27:53 -0600

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -1,44 +1,39 @@
-const worker1 = 'Aaron Aders'
-const worker2 = 'Carie Burnham'
-const worker3 = 'Abel Macias'
-const worker4 = 'Rick Neal'
-const worker5 = 'Mary Vasquez'
-
 const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
 const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
 const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
 
 
-async function fetchFep(contents) {
+async function fetchFep() {
 	const { data, error } = await _supabase
             .from('FEP')
             .select()
 	    .eq('completed', true)
 	if(!error) {
         	//loop display data here
+		let contents = "<ul id=lnamefep>"
         	data.forEach(function(item){
             contents += `<li> ${item.Name}: Date completed ${item.Date}</li>` 
         })
-        
+        contents += "</ul>"
+        document.getElementById("new").innerHTML = contents
     }
-    console.log(data)
     console.log(error)
-    return contents
 }
-async function fetchJCR(contents) {
+async function fetchJCR() {
 	const { data, error } = await _supabase
             .from('connections')
             .select()
 	    .eq('completed', true)
 	if(!error) {
         	//loop display data here
+		let contents = "<ul id=lnamejcr>"
         	data.forEach(function(item){
             contents += `<li> ${item.Name}: Date completed ${item.Date}</li>` 
         })
+	contents += "</ul>"
+        document.getElementById("new").innerHTML = contents
       }
-    console.log(data)
     console.log(error)
-   return contents
 }
 
 
@@ -50,14 +45,14 @@ async function fetchData() {
 	if(!error) {
         	//loop display data here
 		let contents = "<ul id=lname>"
-		contents += fetchFep(contents)
-		contents += fetchJCR(contents)
         	data.forEach(function(item){
             contents += `<li> ${item.Name}: Date completed ${item.Date}</li>` 
         })
 	contents += "</ul>"
         document.getElementById("new").innerHTML = contents
     }
+	fetchFep()
+	fetchJCR()
     console.log(data)
     console.log(error)
 }
