564064435cee641f87d0cb7430ed4f3719004eae
Abel <84032917+AbeMac26@users.noreply.github.com>
2022-06-12 09:57:24 -0600

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -8,6 +8,40 @@ const	DATABASE_URL= 'https://atfurhiavjpgoxvwrjma.supabase.co'
 const	SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZnVyaGlhdmpwZ294dndyam1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODMzNTMsImV4cCI6MTk2OTE1OTM1M30.rW8eEHhilsWb-yKC1o0X4rPhWukF85HEQjcUjjh28IY'
 const _supabase = supabase.createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
 
+
+async function fetchFep(contents) {
+	const { data, error } = await _supabase
+            .from('FEP')
+            .select()
+	    .eq('completed', true)
+	if(!error) {
+        	//loop display data here
+        	data.forEach(function(item){
+            contents += `<li> ${item.Name}: Date completed ${item.Date}</li>` 
+        })
+        
+    }
+    console.log(data)
+    console.log(error)
+    return contents
+}
+async function fetchJCR(contents) {
+	const { data, error } = await _supabase
+            .from('connections')
+            .select()
+	    .eq('completed', true)
+	if(!error) {
+        	//loop display data here
+        	data.forEach(function(item){
+            contents += `<li> ${item.Name}: Date completed ${item.Date}</li>` 
+        })
+      }
+    console.log(data)
+    console.log(error)
+   return contents
+}
+
+
 async function fetchData() {
 	const { data, error } = await _supabase
             .from('CnE')
@@ -15,12 +49,14 @@ async function fetchData() {
 	    .eq('completed', true)
 	if(!error) {
         	//loop display data here
-		let contents = "<ul id=lname>";
+		let contents = "<ul id=lname>"
+		fetchFep(contents)
+		fetchJCR(contents)
         	data.forEach(function(item){
-            contents += `<li> ${item.Name} - ${item.Date}</li>` 
+            contents += `<li> ${item.Name}: Date completed ${item.Date}</li>` 
         })
-	contents += "</ul>";
-        document.getElementById("new").innerHTML = contents;
+	contents += "</ul>"
+        document.getElementById("new").innerHTML = contents
     }
     console.log(data)
     console.log(error)
